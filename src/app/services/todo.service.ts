a8cb05ce2dd4e51791ef416b6c6829f249a40f1a
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-08-31 12:16:30 -0500

    finished todo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -8,18 +8,31 @@ export class TodoService {
   todoList: ITodo [] = [
     // example of how to make an item in todo list
     { title: 'Install Angular CLI', id: this.todoId, status: 'Todo', createdAt: new Date() },
-  
-  ]
+
+  ];
+
+  statuses: string[] = ['todo', 'Doing', 'Done'];
   constructor() { }
-  getTodos(){
-    return this.todoList;
+  getTodos(status?: string): ITodo[] {
+    if (!status) {
+      return this.todoList;
+    }
+
+    return this.todoList.filter(t => t.status === status);
   }
+
   deleteTodo(todo: ITodo) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
+
   addTodo(todo: ITodo): void {
     todo.id = this.todoId ++;
     this.todoList.push(todo);
   }
+
+  getStatuses(): string[] {
+    return this.statuses;
+  }
+
 }
